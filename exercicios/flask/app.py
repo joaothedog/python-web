a4c9cb3ed34546291f4e06ad0987a81fe33293a1
Dog <jvcbatist4@outlook.com>
2024-12-27 12:46:59 -0300

    Template Jinja2 página page

--- exercicios/flask/app.py
+++ exercicios/flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -9,6 +9,10 @@ def home():
 @app.route("/about")
 def about():
     return "Pagina about-me"
+
+@app.route("/page/<nome>")
+def page(nome):
+    return render_template("index.html", nome=nome)
 
 if __name__ == "__main__":
     app.run(debug=True)
